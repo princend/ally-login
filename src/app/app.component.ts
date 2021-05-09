d67215bd90e7aa0921ff19293a2cafe8e5a0e7d3
Princend <waxdji3g4y3c04@gmail.com>
2021-05-09 20:16:45 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { AriaDescriber, LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ally-login';
+export class AppComponent implements AfterViewInit ,OnDestroy{
+  title = '';
+  @ViewChild('account') account: ElementRef<HTMLElement>;
+
+  constructor(private annoucer: LiveAnnouncer, private _ariaDescriber: AriaDescriber) {
+  }
+
+  ngAfterViewInit(): void {
+    this.annoucer.announce('歡迎進入登入頁面', 'assertive');
+    this._ariaDescriber.describe(this.account.nativeElement,'若忘記帳號可以改輸入手機號碼')
+  }
+
+ ngOnDestroy(){
+  this.annoucer.ngOnDestroy() ;
+  this._ariaDescriber.removeDescription(this.account.nativeElement,'若忘記帳號可以改輸入手機號碼');
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {A11yModule} from '@angular/cdk/a11y';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
